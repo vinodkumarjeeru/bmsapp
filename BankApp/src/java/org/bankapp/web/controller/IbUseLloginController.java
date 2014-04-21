c0266b1cee41325284cdccfb534e18dd0b85b30a
amulya8392@gmail.com <amulya8392@gmail.com@b6ce7b63-7b86-955c-3b25-1ccb81f3b004>
2014-04-21 12:22:07 +0000

    added files

---
+++ BankApp/src/java/org/bankapp/web/controller/IbUseLloginController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bankapp.web.controller;
+
+
+
+import org.bankapp.web.utils.RootServlet;
+
+/**
+ *
+ * @author cmc5
+ */
+public class IbUseLloginController extends RootServlet{
+
+}
