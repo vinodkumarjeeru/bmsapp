58b93d920b1148ededfed3af5ec8490d3477b056
zarvis.a@gmail.com <zarvis.a@gmail.com@b6ce7b63-7b86-955c-3b25-1ccb81f3b004>
2014-04-26 02:38:06 +0000

    Added DummyService.java

---
+++ BankApp/src/java/org/bankapp/bankservices/DummyService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.bankapp.bankservices;
+
+import org.bankapp.domain.Dummy;
+
+/**
+ *
+ * @author Sudarsan
+ */
+public interface DummyService {
+
+    void create(Dummy dummy);
+    
+}
