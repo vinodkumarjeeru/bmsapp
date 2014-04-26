82045bfb99e12b1911c93f3454125bc2f55bd0a1
zarvis.a@gmail.com <zarvis.a@gmail.com@b6ce7b63-7b86-955c-3b25-1ccb81f3b004>
2014-04-26 02:58:12 +0000

    Changed DummyServiceImpl.java

--- trunk/BankApp/src/java/org/bankapp/bankservices/impl/DummyServiceImpl.java
+++ trunk/BankApp/src/java/org/bankapp/bankservices/impl/DummyServiceImpl.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.bankapp.bankservices.impl;
 
+import java.util.List;
 import org.bankapp.bankservices.DummyService;
 import org.apache.log4j.Logger;
 import org.bankapp.domain.Dummy;
 import org.bankapp.utils.HibernateUtils;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -21,11 +19,10 @@ public class DummyServiceImpl implements DummyService {
     private static final Logger LOG = Logger.getLogger(DummyServiceImpl.class);
     private static final DummyServiceImpl IMPL = new DummyServiceImpl();
 
-    
-    
     private DummyServiceImpl() {
-        
+
     }
+
     public static DummyService getDummyService() {
         return IMPL;
     }
@@ -51,4 +48,52 @@ public class DummyServiceImpl implements DummyService {
         }
 
     }
+
+    public List<Dummy> getAll() {
+        List<Dummy> list = null;
+
+        Session session = HibernateUtils.currentSession();
+        try {
+            Query query = session.createQuery("from Dummy");
+            list = query.list();
+        } catch (Exception exception) {
+            LOG.warn("", exception);
+        } finally {
+            HibernateUtils.closeSession();
+        }
+        return list;
+    }
+
+    public void delete(Dummy dummy) {
+        Session session = HibernateUtils.currentSession();
+        Transaction tx = null;
+        boolean rollback = true;
+        try {
+            tx = session.beginTransaction();
+            session.delete(dummy);
+            tx.commit();
+            rollback = false;
+        } catch (Exception ex) {
+            LOG.debug("", ex);
+        } finally {
+            if (rollback && tx != null) {
+                tx.rollback();
+            }
+            HibernateUtils.closeSession();
+        }
+    }
+
+    public Dummy getDummy(Long id) {
+        Dummy dummy = null;
+        try {
+            Session session = HibernateUtils.currentSession();
+            dummy = (Dummy) session.get(Dummy.class, id);
+            HibernateUtils.closeSession();
+        } catch (Exception ex) {
+            LOG.debug("Dummy not Found :" + id);
+            LOG.warn("", ex);
+        }
+        return dummy;
+    }
+
 }
